f002b2962215c60c4cdc812e13b1957ca4cb227f
pedro.datasci <pedro.datasci@gmail.com>
2022-09-21 13:21:53 -0300

    funcao de subtracao implementada

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,10 @@
 def soma(n1: int, n2: int) -> int:
     return n1 + n2
 
+def subtracao(x, y):
+	resultado = x - y
+	return resultado
+
 if __name__ == '__main__':
     print(soma(1,1))
+    print(subtracao(1,1))
